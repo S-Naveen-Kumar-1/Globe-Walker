cd9204758e52a2350318e655df8ec37ee21193cc
S NAVEEN KUMAR <navee1110@gmail.com>
2023-07-21 14:12:08 +0530

    cart page updated

--- SCRIPT/index.js
+++ SCRIPT/index.js
@@ -1,4 +1,29 @@
+//selectors for div above navBar
+let twitter = document.querySelector(".twitterImg");
+let facebook = document.querySelector(".facebookImg");
+let linkedIn = document.querySelector(".linkedInImg");
+let insta = document.querySelector(".instaImg");
+let youTube = document.querySelector(".youTubeImg");
+
+//code for div Above navBar
+twitter.addEventListener("click",()=>{
+    window.open("https://twitter.com/");
+});
+facebook.addEventListener("click",()=>{
+    window.open("https://facebook.com/");
+});
+linkedIn.addEventListener("click",()=>{
+    window.open("https://in.linkedin.com/");
+});
+insta.addEventListener("click",()=>{
+    window.open("https://www.instagram.com/");
+});
+youTube.addEventListener("click",()=>{
+    window.open("https://www.youtube.com/");
+});
+
 // get in touch form deatails storing to local storage
+
 
 let formName=document.getElementById("name");
 let formEmail=document.getElementById("email")
